13f01fc5004e0722131d318c50461ac18d8c99fa
Mateus Silva <Mateus.silva444@outlook.com>
2021-10-09 18:18:52 -0400

    Add project files.

---
+++ Compare/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Compare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbcode1 = new System.Windows.Forms.TextBox();
+            this.tbcode2 = new System.Windows.Forms.TextBox();
+            this.bt_compare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbcode1
+            // 
+            this.tbcode1.Location = new System.Drawing.Point(12, 12);
+            this.tbcode1.Name = "tbcode1";
+            this.tbcode1.Size = new System.Drawing.Size(229, 20);
+            this.tbcode1.TabIndex = 0;
+            this.tbcode1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbcode1_KeyDown);
+            // 
+            // tbcode2
+            // 
+            this.tbcode2.Location = new System.Drawing.Point(12, 38);
+            this.tbcode2.Name = "tbcode2";
+            this.tbcode2.Size = new System.Drawing.Size(229, 20);
+            this.tbcode2.TabIndex = 1;
+            this.tbcode2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbcode2_KeyDown);
+            // 
+            // bt_compare
+            // 
+            this.bt_compare.Location = new System.Drawing.Point(-3, 64);
+            this.bt_compare.Name = "bt_compare";
+            this.bt_compare.Size = new System.Drawing.Size(264, 52);
+            this.bt_compare.TabIndex = 2;
+            this.bt_compare.Text = "Resultado";
+            this.bt_compare.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 111);
+            this.Controls.Add(this.bt_compare);
+            this.Controls.Add(this.tbcode2);
+            this.Controls.Add(this.tbcode1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CompareSN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbcode1;
+        private System.Windows.Forms.TextBox tbcode2;
+        private System.Windows.Forms.Button bt_compare;
+    }
+}
+
---
+++ Compare/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void clear() {
+            bt_compare.UseVisualStyleBackColor = true;
+            bt_compare.FlatStyle = FlatStyle.Standard;
+            bt_compare.FlatAppearance.BorderSize = 1;
+            bt_compare.ForeColor = Color.Black;
+        } 
+        public void tbcode1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter) {
+                SendKeys.Send("{TAB}");
+            }
+        }
+        public void tbcode2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                comparar();
+                SendKeys.Send("{TAB}");
+                SendKeys.Send("{TAB}");
+            }
+        }
+        public void comparar()
+        {
+            clear();
+            string code1 = tbcode1.Text;
+            string code2 = tbcode2.Text;
+
+            if (code1 == code2 && code1.Equals(code2))
+            {
+                bt_compare.BackColor = Color.LightGreen;
+                bt_compare.FlatStyle = FlatStyle.Flat;
+                bt_compare.FlatAppearance.BorderSize = 0;
+            }
+            else
+            {
+                bt_compare.BackColor = Color.FromArgb(255, 33, 37);
+                bt_compare.FlatStyle = FlatStyle.Flat;
+                bt_compare.FlatAppearance.BorderSize = 0;
+            }
+            tbcode1.Clear();
+            tbcode2.Clear();
+
+        }
+    }
+}
